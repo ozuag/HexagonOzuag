89eb9d5a2097494381072c1c03094c3dd05def16
ersin <ersin@pedudi>
2021-04-26 13:49:37 +0300

    Simplification
    Bomba hexagonun bomba ve sayaç görselinin okunurluğu için kullanılan script sadeceleştirildi

--- Assets/Scripts/HexaObjects/DontRotate.cs
+++ Assets/Scripts/HexaObjects/DontRotate.cs
@@ -3,30 +3,11 @@ using UnityEngine;
 
 public class DontRotate : MonoBehaviour
 {
-    [SerializeField]
-    private Transform targetTransform;
-
-    float angleOffSet = 0f;
-    Vector3 _angle = Vector3.zero;
-
-
-    private void Start()
-    {
-        this.angleOffSet = this.transform.localEulerAngles.z;
-    }
-
 
     // Update is called once per frame
     void Update()
     {
-        if (this.targetTransform == null)
-            return;
-
-        if(this.targetTransform.hasChanged)
-        {
-            _angle.z = this.angleOffSet - this.targetTransform.localEulerAngles.z;
-            this.transform.localEulerAngles = _angle;
-
-        }
+       if (this.transform.hasChanged)
+            this.transform.up = Vector3.up;
     }
 }
